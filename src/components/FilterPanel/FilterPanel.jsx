255bb89483bbaa40881a188c4eb4b53d38c81d43
francooo32 <63883342+francooo32@users.noreply.github.com>
2022-05-25 19:13:12 -0300

    create search and filter function

---
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { categoryList, ratingList } from '../../constants/constants';
+import CheckboxProton from '../common/CheckboxProton/CheckboxProton';
+import FilterListToggle from '../common/FilterListToggle/FilterListToggle';
+import SliderProton from '../common/SliderProton/SliderProton';
+import "./FilterPanel.css";
+
+const FilterPanel = ({
+  selectedCategory,
+  selectCategory,
+  selectedRating,
+  selectedPrice,
+  selectRating,
+  cuisines,
+  changeChecked,
+  changePrice,
+}) => (
+  <div>
+    <div className='input-group'>
+      <p className='label'>Category</p>
+      <FilterListToggle
+        options={categoryList}
+        value={selectedCategory}
+        selectToggle={selectCategory}
+      />
+    </div>
+    <div className='input-group'>
+      <p className='label'>Marca</p>
+      {cuisines.map((cuisine) => (
+        <CheckboxProton
+          key={cuisine.id}
+          cuisine={cuisine}
+          changeChecked={changeChecked}
+        />
+      ))}
+    </div>
+    <div className='input-group'>
+      <p className='label-range'>Price Range</p>
+      <SliderProton value={selectedPrice} changePrice={changePrice} />
+    </div>
+    <div className='input-group'>
+      <p className='label'>Star Rating</p>
+      <FilterListToggle
+        options={ratingList}
+        value={selectedRating}
+        selectToggle={selectRating}
+      />
+    </div>
+  </div>
+);
+
+export default FilterPanel;
