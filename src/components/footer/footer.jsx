61c9f1e02d5c899d0da91cfe6e03ee86b3ce6b58
francooo32 <63883342+francooo32@users.noreply.github.com>
2023-10-08 19:24:39 -0300

    Release Vr 1.0

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
-import "./footer.css"
 import logoPhoto from "../../public/Logos/2.png"
 import navIcon1 from "../../public/footerImg/nav-icon1.svg";
 import navIcon2 from "../../public/footerImg/nav-icon2.svg";
 import navIcon3 from "../../public/footerImg/nav-icon3.svg";
+import "./footer.css"
 
 function Footer() {
   return (
@@ -13,12 +13,26 @@ function Footer() {
           {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
             <img id="footerImg" src={logoPhoto} alt="Logo" />
+            <ul>
+              <li>
+                <a href="/comprar">Comprar</a>
+              </li>
+              <li>
+                <a href="/vender">Vender</a>
+              </li>
+              <li>
+                <a href="/nosotros">Nosotros</a>
+              </li>
+              <li>
+                <a href="/contactUs">Contacto</a>
+              </li>
+            </ul>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.instagram.com/vittoria_automobili/"  target="_blank" ><img src={navIcon3} alt="Icon"/></a>
             </div>
             <div className="copyright">
               <p>Copyright 2023. All Rights Reserved</p>
